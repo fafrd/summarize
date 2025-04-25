d739a536719df7fc7842e9addefece358fef7a84
kian <fafrd@users.noreply.github.com>
2025-04-25 04:38:43 +0000

    update model

--- api/summarizer.py
+++ api/summarizer.py
@@ -5,16 +5,15 @@ from vertexai.generative_models import GenerativeModel, GenerationConfig
 
 vertexai.init(project='elegant-gearing-417520', location='us-central1')
 
-gemini_model = GenerativeModel("gemini-2.0-flash-thinking-exp-01-21")
+#gemini_model = GenerativeModel("gemini-2.0-flash-thinking-exp-01-21")
+gemini_model = GenerativeModel("gemini-2.5-flash-preview")
 generation_config = GenerationConfig(
     temperature=0.7,
-    max_output_tokens=8192,
+    max_output_tokens=16384,
 )
 
 def summarize_transcript(transcript: str) -> str:
-    """Generates a summary of the transcript using an LLM."""
-
-    prompt = f"Please summarize this Youtube video. Focus on the main takeaways, summarizing them. When participants take specific positions, mention it. Look for anything controversial or out of the ordinary.\n\n{transcript}\n"
+    prompt = f"Please summarize this video. Focus on the main takeaways, summarizing them. Start high level, then go into the details. When participants take specific positions, mention it. As always, keep an eye out for anything unusual or out of the especially notable.\n\n{transcript}\n"
 
     model_response = gemini_model.generate_content(prompt, generation_config=generation_config)
 
